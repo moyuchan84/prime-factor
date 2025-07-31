012c4ee6f4a1b215301f9d66da114fde1aa51ba3
yuchan <mo.yuchan.84@gmail.com>
2025-07-31 12:39:39 +0900

    [feature] p30-31 추가 테스트케이스(2) 에 대한 구현

--- prime_factors.py
+++ prime_factors.py
@@ -1,6 +1,8 @@
 class PrimeFactor():
     ...
 
-    def of(self, param)-> []:
+    def of(self, number)-> []:
         factors = []
+        if number == 2:
+            factors.append(2)
         return factors

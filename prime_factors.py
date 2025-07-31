c176d756d7f8835e9d4287fcabff222018e32b03
yuchan <mo.yuchan.84@gmail.com>
2025-07-31 13:01:47 +0900

    [feature] p53 로직일반화

--- prime_factors.py
+++ prime_factors.py
@@ -3,15 +3,13 @@ class PrimeFactor():
 
     def of(self, number)-> []:
         factors = []
-        if number > 1:
-            divisor = 2
-            if number in [4,6,9,12]:
-                divisor = 2
-                while number > 1:
-                    while number % divisor == 0:
-                        factors.append(divisor)
-                        number //= divisor
-                    divisor+=1
-            else:
-                factors.append(number)
+
+        divisor = 2
+
+        while number > 1:
+            while number % divisor == 0:
+                factors.append(divisor)
+                number //= divisor
+            divisor+=1
+
         return factors

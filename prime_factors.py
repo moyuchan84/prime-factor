e61a67382c4496f7b1a7b9eac11ea81cb850aa66
yuchan <mo.yuchan.84@gmail.com>
2025-07-31 12:47:01 +0900

    [refactor] p40  로직 일반화

--- prime_factors.py
+++ prime_factors.py
@@ -5,12 +5,10 @@ class PrimeFactor():
         factors = []
         if number > 1:
             if number == 4:
-                if number % 2 ==0:
+                while number % 2 ==0:
                     factors.append(2)
                     number //=2
-                if number % 2 == 0:
-                    factors.append(2)
-                    number //=2
+
             else:
                 factors.append(number)
         return factors

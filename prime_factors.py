d80bb2341db5a5931d7dccd146018ddadc88b477
yuchan <mo.yuchan.84@gmail.com>
2025-07-31 12:54:21 +0900

    [refactor] p46 중복제거

--- prime_factors.py
+++ prime_factors.py
@@ -10,14 +10,12 @@ class PrimeFactor():
                     factors.append(divisor)
                     number //= divisor
             elif number == 6:
-                while number % divisor == 0:
-                    factors.append(divisor)
-                    number //= divisor
-                divisor+=1
-                while number % divisor ==0:
-                    factors.append(divisor)
-                    number //=divisor
-                divisor += 1
+                divisor = 2
+                while number > 1:
+                    while number % divisor == 0:
+                        factors.append(divisor)
+                        number //= divisor
+                    divisor+=1
             else:
                 factors.append(number)
         return factors

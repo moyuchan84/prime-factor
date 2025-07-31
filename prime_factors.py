b04c63520269353bf9476341a4512d7290fc2fd7
yuchan <mo.yuchan.84@gmail.com>
2025-07-31 12:59:36 +0900

    [refactor] p50 4,6,9 케이스에 대한 일반화

--- prime_factors.py
+++ prime_factors.py
@@ -5,11 +5,7 @@ class PrimeFactor():
         factors = []
         if number > 1:
             divisor = 2
-            if number == 4:
-                while number % divisor ==0:
-                    factors.append(divisor)
-                    number //= divisor
-            elif number in [6,9]:
+            if number in [4,6,9]:
                 divisor = 2
                 while number > 1:
                     while number % divisor == 0:
